98046fe55fd19f3614bfd8366ec622dcf56a647c
Lucas <lucassetem@gmail.com>
2023-07-08 22:00:34 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var n1 = 5;
+var n2 = 10;
+var media;
+var res;
+media = (n1+n2)/2;
+if(media >= 7){
+    res = "aprovado."
+}
+else{
+    res = "reprovado."
+}
+alert("Sua nota final é "+ media + ". Você está " + res)
